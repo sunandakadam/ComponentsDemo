024caf08c6e96b6f6120a41c7c9d434954abd225
sunandakadam <sunandak2007@gmail.com>
2023-04-27 11:49:11 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';  
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
 
@@ -10,7 +11,9 @@ import { ChildComponent } from './child/child.component';
     ChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
